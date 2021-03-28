1aff37ddd92acb54916f318f165d55f39f15f58f
felipesantos10 <felipedev.ti@gmail.com>
2021-03-28 16:34:53 -0300

    primeiro commit

---
+++ mundoUM/ex001.py
@@ -0,0 +1,8 @@
+#cria um programa que escreva "ola, mundo" na tela
+
+#exemplo 1
+print("Olá, Mundo!")
+
+#exemplo 2
+msg = 'ola, Mundo!'
+print(msg)
---
+++ mundoUM/ex002.py
@@ -0,0 +1,10 @@
+#Faça um programa que leia o nome de uma pessoa 
+# e mostre uma mensagem de boas-vindas.
+
+#exemplo 1
+nome = input("Qual o seu nome?")
+print("Prazer em te conhecer, ",nome)
+
+#exemplo 2
+nome = input("Qual o seu nome?")
+print("Prazer em te conhecer, {}".format(nome))
