f4f3d8f7038df8cb3f6d7406c4716ad9b2fa8292
felipesantos10 <felipedev.ti@gmail.com>
2021-03-30 16:45:08 -0300

    aula nova

---
+++ ex003.py
@@ -0,0 +1,13 @@
+#Cria um programa que leia dois numeros e mostre a soma entre eles
+
+#exemplo 1
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Digite um outro numero: '))
+s = (n1 + n2)
+print('A soma entre {}  e {} é {}'.format(n1,n2,s))
+
+#exemplo2
+n1 = float(input('Digite um numero: '))
+n2 = float(input('Digite um outro numero: '))
+s = (n1 + n2)
+print('A soma entre {}  e {} é {}'.format(n1,n2,s))
---
+++ mundoUM/aula6.py
@@ -0,0 +1,21 @@
+#Tipos primitivos - anotações
+
+# Int = 7, -7, 0, 9875
+# float = 4.5, 0.0076, -15.223
+# bool = true, false
+# str = 'ola', '7', '4.6'
+
+# Duas formas de aplicar o print
+# print('a soma vale',s)
+# print('a soma vale().format(s))
+
+# Para mostrar os tipos de uma variavel,colocar print(type())
+# print(n.isnumeric())
+# print(n.isalpha())
+# print(n.isalnum)
+# print(n.isupper)
+# print(n.)
+n1 = int(input('digite um valor: '))
+n2 = int(input('digite um outro valor:'))
+
+print('a ')
