3107314619194207a191f56b1cc94c26adb1f8bc
felipesantos10 <felipedev.ti@gmail.com>
2021-04-27 21:59:09 -0300

    exercicio string

---
+++ mundoUM/ex027.py
@@ -0,0 +1,6 @@
+#Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
+
+n = str(input('Informe o seu nome completo: ')).strip()
+nome = n.split()
+print('O seu primeiro nome é {}'.format(nome[0]))
+print('O seu ultimo nome é {}'.format(nome[len(nome)-1]))
