d7161a4f35131c61363940a879b2440bce49bd14
felipesantos10 <felipedev.ti@gmail.com>
2021-05-21 18:57:01 -0300

    novos exercicios

--- mundoDois/ex048.py
+++ mundoDois/ex048.py
@@ -1,6 +1,8 @@
+#Faça um programa que calcule a soma entre todos os números que são múltiplos de três e que se encontram no intervalo de 1 até 500.
 s = 0
-for impar in range(0,500,3):
-    print(impar)
-    s = s+impar
-print(s)
+for impar in range(1,500,2):
+    if impar % 3 ==0:
+        print(impar)
+        s = s+impar
+print('o somatorio de todos os valores impar foi {}'.format(s))
 
---
+++ mundoDois/ex049.py
@@ -0,0 +1,4 @@
+num = int(input('Digite um numero para ver sua tabuada: '))
+for c in range(0,10+1):
+    print('{} x {} = {}'.format(num,c,num*c))
+
